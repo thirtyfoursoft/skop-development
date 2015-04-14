690c218d48b479851db754994eba0542586391d7
info@thirtyfour.in <sunildesign@34>
2015-04-14 15:52:42 +0530

    update wp-config.php file
    update wp-config.php file

--- wp-config.php
+++ wp-config.php
@@ -16,13 +16,13 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'sandbox6_skopes');
+define('DB_NAME', 'sandbox7_wpdb');
 
 /** MySQL database username */
-define('DB_USER', 'sandbox6_skopes');
+define('DB_USER', 'sandbox7_lcubed');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'Rvtech4you#');
+define('DB_PASSWORD', '5@nd60X');
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
